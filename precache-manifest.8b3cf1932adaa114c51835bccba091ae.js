990ee89e7895413687ce4a7ad5430480cd729b24
Ivan Caviedes <ivancaviedes99@gmail.com>
2020-06-10 05:46:59 -0500

    Updates

---
+++ precache-manifest.8b3cf1932adaa114c51835bccba091ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03a0e6dbcc8f28c1e543b5c5c664313",
+    "url": "/HomePagePortafolio/index.html"
+  },
+  {
+    "revision": "6170c9fd0b7864ab2052",
+    "url": "/HomePagePortafolio/static/js/2.9ae69465.chunk.js"
+  },
+  {
+    "revision": "9caebd9c6bad93f2eb60fdf2a2eb09a2",
+    "url": "/HomePagePortafolio/static/js/2.9ae69465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f651c571aa3fff2cff02",
+    "url": "/HomePagePortafolio/static/js/main.ef4d0f98.chunk.js"
+  },
+  {
+    "revision": "ecba636222665ea00cce",
+    "url": "/HomePagePortafolio/static/js/runtime-main.bb1b5ede.js"
+  }
+]);
